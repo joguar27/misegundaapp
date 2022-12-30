0af3609f5bedb2dfdeea0d73b700709ab26fc187
jorge <jorgegusarruda@gmail.com>
2022-12-30 19:22:41 -0300

    organizo archivos iniciales de instalacion

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola Jorge!!!</h1>
-        <h2>Soy mi segunda App en React!!!</h2>
-        <p>Aloha!!!</p>
-        <p>
-          <code>Jorge Arruda | misegundaapp</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendiendo React Js
-        </a>
-      </header>
-    </div>
-  );
+import Card from './Card'
+const App=()=>{
+  return(
+    <>
+        <Card/>
+        <Card></Card>
+    </>
+  )
 }
-
-export default App;
+export default App
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+const Card=()=>{
+    return(
+        <article>
+            <h2>Card</h2>
+            <p>Card content</p>
+        </article>
+    )
+}
+export default Card
