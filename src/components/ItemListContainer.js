0592f2c5c29c54ba5a611eb2d41e8db7ccbfae3d
jorge <jorgegusarruda@gmail.com>
2023-01-04 17:59:57 -0300

    agregue contador y carrito en el navbar

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,6 +1,21 @@
-const ItemListContainer = (props) => {
+import { useState } from 'react'
+
+    const ItemListContainer = (props) => {
+
+    // const miEstado = useState(0)
+    // const contador = miEstado[0]
+    // const cambiarContador = miEstado[1]
+
+    const [contador, setContador] = useState(0)
+    const handleClick = () => {
+        setContador(contador + 1)
+    }
     return (
-        <div>{props.greeting}</div>
+        <div>{props.greeting}
+            <p>Contador: {contador}</p>
+            <button onClick={handleClick} >Sumar</button>
+        </div>
     )
 }
 export default ItemListContainer
+
